bc2083176bc3111ceb1910ed99a5284b9e9ac5c9
Richard Mitchell <rwmitchell@gmail.com>
2022-04-07 10:18:35 -0400

    add: Keywords to --version output
    • Makefile:
       ◦ add ‘columns’ to format filename output
       ◦ make all *.o files dependent on all *.h files
    
    • version.h:
       ◦ add my keywords to VersionID string

--- Source/version.h
+++ Source/version.h
@@ -3,6 +3,12 @@ static char *VersionID = "\n\
 @(#) Author: Mark Baranowski\n\
 @(#) Email: requestXXX@els-software.org (remove XXX)\n\
 @(#) Download: http://els-software.org\n\
+\n\
+$Id$\n\
+$MyId$\n\
+$Date$\n\
+$Source$\n\
+$Auth$\n\
 ";
 
 #define  VERSION_LEVEL  154
